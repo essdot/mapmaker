cbbc977eebc65e5c5a27adefa221f25adae73917
Justin Sippel essdot <justinsippel+gh@gmail.com>
2015-07-06 22:40:41 -0700

    Spacing

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 module.exports = mapmaker
 
 function mapmaker(mapSpec) {
-	if(mapSpec === null || typeof mapSpec !== 'object') {
-		throw new TypeError('mapSpec must be an object')
-	}
+  if(mapSpec === null || typeof mapSpec !== 'object') {
+    throw new TypeError('mapSpec must be an object')
+  }
 
-	if(isIterable(mapSpec)) {
-		return new Map(mapSpec)
-	}
+  if(isIterable(mapSpec)) {
+    return new Map(mapSpec)
+  }
 
-	return new Map(
-		Object.keys(mapSpec).map(function(key) {
-			return [key, mapSpec[key]]
-		})
-	)
+  return new Map(
+    Object.keys(mapSpec).map(function(key) {
+      return [key, mapSpec[key]]
+    })
+  )
 }
 
 function isIterable(obj){
-	if(typeof Symbol === 'undefined') {
-		return obj instanceof Map
-	}
+  if(typeof Symbol === 'undefined') {
+    return obj instanceof Map
+  }
 
-	return typeof obj[Symbol.iterator] === 'function'
+  return typeof obj[Symbol.iterator] === 'function'
 }
--- test.js
+++ test.js
@@ -4,49 +4,49 @@ const test = require('tape')
 const mapmaker = require('./index')
 
 test('returns a map', function(t) {
-	const map = mapmaker({abc: 'def', 123: 456})
-	const keys = []
-	const iter = map.keys()
+  const map = mapmaker({abc: 'def', 123: 456})
+  const keys = []
+  const iter = map.keys()
 
-	keys.push(iter.next().value)
-	keys.push(iter.next().value)
-	keys.sort()
+  keys.push(iter.next().value)
+  keys.push(iter.next().value)
+  keys.sort()
 
-	t.ok(map instanceof Map)
-	t.deepEqual(keys, ['123', 'abc'])
-	t.equal(map.get('abc'), 'def')
-	t.equal(map.get('123'), 456)
-	t.equal(map.size, 2)
+  t.ok(map instanceof Map)
+  t.deepEqual(keys, ['123', 'abc'])
+  t.equal(map.get('abc'), 'def')
+  t.equal(map.get('123'), 456)
+  t.equal(map.size, 2)
 
-	t.ok(map.has('123'))
-	t.notOk(map.has('456'))
+  t.ok(map.has('123'))
+  t.notOk(map.has('456'))
 
-	t.end()
+  t.end()
 })
 
 test('works for iterables', function(t) {
-	const map = mapmaker({abc: 'def', 123: 456})
-	const mapmap = mapmaker(map)
+  const map = mapmaker({abc: 'def', 123: 456})
+  const mapmap = mapmaker(map)
 
-	const setmap = mapmaker(new Set([
-		['abc', 'def'], [123, 456]
-	]))
+  const setmap = mapmaker(new Set([
+    ['abc', 'def'], [123, 456]
+  ]))
 
-	t.ok(map instanceof Map)
-	t.ok(mapmap instanceof Map)
-	t.equal(mapmap.size, 2)
-	t.ok(setmap instanceof Map)
-	t.equal(mapmap.size, 2)
+  t.ok(map instanceof Map)
+  t.ok(mapmap instanceof Map)
+  t.equal(mapmap.size, 2)
+  t.ok(setmap instanceof Map)
+  t.equal(mapmap.size, 2)
 
-	t.end()
+  t.end()
 })
 
 test('TypeError for non-object', function(t) {
-	t.throws(mapmaker.bind(null, 2), TypeError)
-	t.throws(mapmaker.bind(null, undefined), TypeError)
-	t.throws(mapmaker.bind(null, null), TypeError)
-	t.throws(mapmaker.bind(null, 'str'), TypeError)
-	t.throws(mapmaker.bind(null, true), TypeError)
+  t.throws(mapmaker.bind(null, 2), TypeError)
+  t.throws(mapmaker.bind(null, undefined), TypeError)
+  t.throws(mapmaker.bind(null, null), TypeError)
+  t.throws(mapmaker.bind(null, 'str'), TypeError)
+  t.throws(mapmaker.bind(null, true), TypeError)
 
-	t.end()
+  t.end()
 })
